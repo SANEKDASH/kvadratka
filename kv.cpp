6058b4578c0b018405aec11c72d962992ec08688
SANEKDASH <dashinsky_alexander92@mail.ru>
2023-08-12 16:14:10 +0300

    +structures, some func

--- kv.cpp
+++ kv.cpp
@@ -1,102 +1,169 @@
 #include <stdio.h>
 #include <math.h>
-
-
-double discriminate (double a, double b, double c)
+//
+typedef enum
 {
-    double d;
+    kZeroRoots = 0,
+    kOneRoot  = 1,
+    kTwoRoots  = 2,
+    kInfRoots  = 3,
+} RootsCount_t;
+//
 
-    d = pow(b, 2) - 4 * a * c;
+typedef struct
+{
+    double a;
+    double b;
+    double c;
+} sq_coeffs;
+//
 
-    return d;
+typedef struct
+{
+    double x1;
+    double x2;
+} salvs;
+//
+
+double find_dval(sq_coeffs cur_cfs)
+{
+    return pow(cur_cfs.b, 2) - 4 * cur_cfs.a * cur_cfs.c;
 }
 
-double sq_solve (double a, double b, double c, double d, double mn)
+
+void solve_sq(sq_coeffs cur_cfs, double d, salvs* cur_salvs)
 {
-    return (-b + mn * sqrt(d)) / (2 * a);
+    cur_salvs->x1 = (-cur_cfs.b + sqrt(d)) / (2 * cur_cfs.a);
+    cur_salvs->x2 = (-cur_cfs.b - sqrt(d)) / (2 * cur_cfs.a);
 }
 
-double line_solve(double b, double c)
+
+double solve_linear(sq_coeffs cur_cfs, salvs* cur_salvs)
 {
-    return -c / b;
+    cur_salvs->x1 = cur_salvs->x2 = -cur_cfs.c / cur_cfs.b;
 }
 
-void read_coeff(double *a, const char *s)
-{
-    printf("enter '%s' coefficient value:  ", s);
-    scanf("%lf", a);
 
+int read_cfs(sq_coeffs* coeffs)
+{
+    printf("enter A coeff. value: ");
+    while(scanf("%lf", &coeffs->a) < 0 )
+        printf("enter A coeff. value: ");
+
+    printf("enter B coeff. value: ");
+    while(scanf("%lf", &coeffs->b) < 0 )
+        printf("enter A coeff. value: ");
+
+    printf("enter C coeff. value: ");
+    while(scanf("%lf", &coeffs->c) < 0 )
+        printf("enter A coeff. value: ");
 }
 
-main()
+
+RootsCount_t solve_equasion(sq_coeffs cur_cfs,
+                            double discr,
+                            salvs* cur_salvs)
 {
-    double a = 0;
-    double b = 0;
-    double c = 0;
-
-    read_coeff(&a, "a");
-    read_coeff(&b, "b");
-    read_coeff(&c, "c");
-
-    printf("\nA = %0.2f B = %0.2f C = %0.2f\n", a, b, c);
-
-    printf("____________________________________\n");
-
-
-    double d = 0;
-
-    d = discriminate(a, b, c);
-
-
-    if (d < 0)
+    if (discr < 0)
     {
-        printf("there are no salvations\n");
+        return kZeroRoots;
     }
-    else if (a != 0)
+    else if (cur_cfs.a != 0)
     {
-        double x1 = 0;
-        double x2 = 0;
 
-        x1 = sq_solve(a, b, c, d, 1);
-        x2 = sq_solve(a, b, c, d, -1);
+        solve_sq(cur_cfs, discr, cur_salvs);
 
-        if (x1 == x2)
+        if (cur_salvs->x1 == cur_salvs->x2)
         {
-            printf("there is one root:\n");
-            printf("\tx = %.2lf\n",x1 );
+            return kOneRoot;
         }
         else
         {
-            printf("there are two roots:\n");
-            printf("\tx1 = %.2lf\n\tx2 = %.2lf\n", x1, x2);
+            return kTwoRoots;
 
         }
     }
     else
     {
-        if (b == 0)
+        if (cur_cfs.b == 0)
         {
-            if (c == 0)
+            if (cur_cfs.c == 0)
             {
-                printf("x belongs to the set of all numbers\n");
+                return kInfRoots;
             }
             else
             {
-                printf("there are no roots\n");
+                return kZeroRoots;
             }
         }
         else
         {
-            double x;
 
-            x = line_solve(b, c);
 
-            printf("there is one root:\n");
-            printf("\tx = %.2lf\n", x);
+            solve_linear(cur_cfs, cur_salvs);
+
+            return kOneRoot;
         }
     }
 
+
+}
+
+
+void print_output(sq_coeffs cur_cfs, double discr, salvs cur_salvs, RootsCount_t mode)
+{
     printf("____________________________________\n");
+    printf("\nA = %0.2lf | B = %0.2lf | C = %0.2lf\n", cur_cfs.a, cur_cfs.b, cur_cfs.c);
+    printf("\tD = %.2lf\n", discr);
+    printf("____________________________________\n");
+
+    switch (mode)
+    {
+        case kZeroRoots:
+            printf("there are no any solutions\n");
+            break;
+        case kOneRoot:
+            printf("there is one solution:\n");
+            printf("\tx = %.2lf", cur_salvs.x1);
+            break;
+        case kTwoRoots:
+            printf("there are two roots:\n");
+            printf("\tx1 = %.2lf\n\tx2 = %.2lf\n", cur_salvs.x1, cur_salvs.x2);
+            break;
+        case kInfRoots:
+            printf("there are a lot of roots :)\n");
+            break;
+        default:
+            printf("what the fuck!?\n");
+    }
+
+    printf("____________________________________\n");
+}
+
+
+
+main()
+{
+
+    sq_coeffs cur_cfs;
+
+    cur_cfs.a = 0;
+    cur_cfs.c = 0;
+    cur_cfs.b = 0;
+
+    read_cfs(&cur_cfs);
+
+    double d = 0;
+    d = find_dval(cur_cfs);
+
+    RootsCount_t mode;
+    salvs cur_salvs;
+
+    mode = solve_equasion(cur_cfs, d, &cur_salvs);
+
+    print_output(cur_cfs, d, cur_salvs, mode);
+
+
 
     return 0;
 
